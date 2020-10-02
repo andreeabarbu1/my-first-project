629406daf5e75a40d37c3b4968d7f63c6c5425a5
andreeabarbu1 <andreeabarbu169@gmail.com>
2020-10-03 00:09:50 +0300

    new add

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -34,7 +34,7 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-(function (window) {
+(function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -67,5 +67,5 @@ for (var currentName = 0; currentName < names.length; currentName++) {
 }
 
 // the end of the IIFE
-}) (window);
+}) ();
 
